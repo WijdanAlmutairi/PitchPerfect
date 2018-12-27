27bd45e582e924fabe4d504b394b8723613fd642
Wijdan <wjdanalmoteri@gmail.com>
2018-12-27 04:41:53 +0300

    feat: complete code for the first project
    This is the final version of the project pitch perfect

--- PitchPerfect/PlaySoundsViewController+Audio.swift
+++ PitchPerfect/PlaySoundsViewController+Audio.swift
@@ -8,7 +8,7 @@
 import UIKit
 import AVFoundation
 
-// MARK: - PlaySoundsViewController: AVAudioPlayerDelegate
+ // MARK: - PlaySoundsViewController: AVAudioPlayerDelegate
 
 extension PlaySoundsViewController: AVAudioPlayerDelegate {
     
